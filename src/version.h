7e8462d6a92766ddad73437a677efb14f59f75c1
infernoman <jjaauto@hotmail.com>
2018-05-29 20:19:47 -0400

    update protocol version cutoff

--- src/version.h
+++ src/version.h
@@ -36,17 +36,17 @@ static const int PROTOCOL_VERSION = 60029;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 60027;
+static const int MIN_PEER_PROTO_VERSION = 60029;
 
 // minimum peer version accepted by DarkSendPool
-static const int MIN_POOL_PEER_PROTO_VERSION = 60027; 
+static const int MIN_POOL_PEER_PROTO_VERSION = 60029; 
 
-static const int MIN_INSTANTX_PROTO_VERSION = 60027;
+static const int MIN_INSTANTX_PROTO_VERSION = 60029;
 
 //! minimum peer version that can receive masternode payments
 // V1 - Last protocol version before update
 // V2 - Newest protocol version
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 60027;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 60029;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
